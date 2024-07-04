cea18f81b47fd4f1bf2c9f5ce257d99e398bbb24
s1F102301628 <s1f102301628@iniad.org>
2024-07-04 11:23:43 +0900

    エラー解消

--- todo/tests.py
+++ todo/tests.py
@@ -3,10 +3,12 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
-# Create your tests here.
+
+
 class SampleTestCase(TestCase):
     def test_sample1(self):
         self.assertEqual(1 + 2, 3)
+
 
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
@@ -36,6 +38,7 @@ class TaskModelTestCase(TestCase):
 
         self.assertFalse(task.is_overdue(current))
 
+
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
         client = Client()
@@ -47,7 +50,7 @@ class TodoViewTestCase(TestCase):
 
     def test_index_post(self):
         client = Client()
-        data = {'title': 'Test Task', 'due_at':'2024-06-30 23:59:59'}
+        data = {'title': 'Test Task', 'due_at': '2024-06-30 23:59:59'}
         response = client.post('/', data)
 
         self.assertEqual(response.status_code, 200)
@@ -79,3 +82,4 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(response.context['tasks'][0], task1)
         self.assertEqual(response.context['tasks'][1], task2)
+        
